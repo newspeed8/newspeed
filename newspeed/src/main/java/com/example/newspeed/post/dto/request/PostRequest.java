59974759f492c2d5d7dafb091ae5d0359d91af7e
sjMun09 <ohoh7391@naver.com>
2025-02-16 11:41:07 +0900

    feat:게시물 기능 개발완료

--- newspeed/src/main/java/com/example/newspeed/post/dto/request/PostRequest.java
+++ newspeed/src/main/java/com/example/newspeed/post/dto/request/PostRequest.java
@@ -1,10 +1,12 @@
 package com.example.newspeed.post.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PostRequest (
-    @NotBlank String title,
-    @NotBlank String content,
-    @NotBlank String imageUrl,
-    @NotBlank String nickname2
+    @NotBlank(message = "제목은 필수입니다.") String title,
+    @NotBlank(message = "내용은 필수입니다.") String content,
+    @NotBlank(message = "이미지 URL은 필수입니다.") String imageUrl,
+    @NotBlank(message = "닉네임은 필수입니다.") String nickname2,
+    @NotNull(message = "userId는 필수입니다.") Long userId
 ){}
