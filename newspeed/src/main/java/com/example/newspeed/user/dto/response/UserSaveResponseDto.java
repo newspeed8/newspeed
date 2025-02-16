c11f1cd3a383056ac356ac34fd992e88e7c7fb61
minkyungCHO <mk981015@naver.com>
2025-02-17 08:01:55 +0900

    feat: UserSaveResponseDto 생성

---
+++ newspeed/src/main/java/com/example/newspeed/user/dto/response/UserSaveResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.newspeed.user.dto.response;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserSaveResponseDto {
+
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public UserSaveResponseDto(Long id, String name, String email, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
