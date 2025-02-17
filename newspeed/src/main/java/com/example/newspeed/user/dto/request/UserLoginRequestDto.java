2a20ed70670edd2ff1e6ea40b0d26e53e74115b1
JoeMinKyung <mk981015@naver.com>
2025-02-17 11:40:55 +0900

    feat: 이메일 형식, 비밀번호 입력 유효성 검사

--- newspeed/src/main/java/com/example/newspeed/user/dto/request/UserLoginRequestDto.java
+++ newspeed/src/main/java/com/example/newspeed/user/dto/request/UserLoginRequestDto.java
@@ -1,10 +1,15 @@
 package com.example.newspeed.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UserLoginRequestDto {
 
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 }
