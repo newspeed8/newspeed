5f49b366a7292de74c275514b5cdf40c15358f26
JoeMinKyung <mk981015@naver.com>
2025-02-17 15:07:19 +0900

    feat: 본인 게시물에는 좋아요 누를 수 없게 수정

--- newspeed/src/main/java/com/example/newspeed/like/service/LikeService.java
+++ newspeed/src/main/java/com/example/newspeed/like/service/LikeService.java
@@ -29,6 +29,10 @@ public class LikeService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new IllegalStateException("게시물을 찾을 수 없습니다."));
 
+        if (post.getUser().getId().equals(userId)) {
+            throw new IllegalStateException("본인 게시물에는 좋아요를 누를 수 없습니다.");
+        }
+
         boolean liked;
         Optional<Like> existingLike = likeRepository.findByUserAndPost(user, post);
         if (existingLike.isPresent()) {
