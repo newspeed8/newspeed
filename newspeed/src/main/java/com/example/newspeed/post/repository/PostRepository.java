353331bd687920b7f7ed79391ac0b2fd51d51cc7
HoKirby <lakevely27@gmail.com>
2025-02-17 15:35:10 +0900

    feat: 친구 최신 게시물 최신순으로 조회 기능 추가
    JPQL 쿼리 작성으로 Friend table 데이터 가져옴
    사용되지 않는 변수 제거

--- newspeed/src/main/java/com/example/newspeed/post/repository/PostRepository.java
+++ newspeed/src/main/java/com/example/newspeed/post/repository/PostRepository.java
@@ -2,11 +2,21 @@ package com.example.newspeed.post.repository;
 
 import com.example.newspeed.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Query("""
+    SELECT p FROM Post p
+    JOIN Friend f ON p.user.id = f.requester.id
+    WHERE p.user.id = :uerId
+    AND f.status = 'ACCEPTED'
+    ORDER BY p.createdAt DESC
+    """)
+    List<Post> findFriendPostsByUserId(@Param("userId") Long userId);
     // 유저 ID로 모든 게시물을 조회하는 메소드
     List<Post> findByUserId_Id(Long userId);
 }
