1a8bdb33c303393cfa62f297f77b5d7d4ac56a32
JoeMinKyung <mk981015@naver.com>
2025-02-17 09:10:54 +0900

    feat: create UserLoginResponseDto

---
+++ newspeed/src/main/java/com/example/newspeed/user/dto/response/UserLoginResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.newspeed.user.dto.response;
+
+public class UserLoginResponseDto {
+
+    private final Long userId;
+
+    public UserLoginResponseDto(Long userId) {
+        this.userId = userId;
+    }
+}
