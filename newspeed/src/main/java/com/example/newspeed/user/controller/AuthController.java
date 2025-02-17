25c56bbb308c541de635226dbec125a1acd484e9
JoeMinKyung <mk981015@naver.com>
2025-02-17 09:13:02 +0900

    feat: login

---
+++ newspeed/src/main/java/com/example/newspeed/user/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.newspeed.user.controller;
+
+import com.example.newspeed.user.dto.request.UserLoginRequestDto;
+import com.example.newspeed.user.dto.response.UserLoginResponseDto;
+import com.example.newspeed.user.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @RequestBody UserLoginRequestDto dto,
+            HttpServletRequest request
+    ) {
+
+        UserLoginResponseDto result = authService.login(dto);
+        return ResponseEntity.ok("로그인 성공");
+    }
+}
---
+++ newspeed/src/main/java/com/example/newspeed/user/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.newspeed.user.service;
+
+import com.example.newspeed.user.dto.request.UserLoginRequestDto;
+import com.example.newspeed.user.dto.response.UserLoginResponseDto;
+import com.example.newspeed.user.entity.User;
+import com.example.newspeed.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserLoginResponseDto login(UserLoginRequestDto dto) {
+        User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(
+                () -> new IllegalStateException("이메일이 존재하지 않습니다.")
+        );
+
+        String password = dto.getPassword();
+        if (password == null) {
+            throw new IllegalStateException("비밀번호를 입력해주세요.");
+        }
+
+        if (!password.equals(user.getPassword())) {
+            throw new IllegalStateException("비밀번호가 틀렸습니다.");
+        }
+        return new UserLoginResponseDto(user.getId());
+    }
+
+}
