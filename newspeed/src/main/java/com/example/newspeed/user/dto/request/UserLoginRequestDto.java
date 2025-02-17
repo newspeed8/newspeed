9ed8d2affdc0b635bbdd925be7989f249e652a93
JoeMinKyung <mk981015@naver.com>
2025-02-17 09:10:41 +0900

    feat: create UserLoginRequestDto

---
+++ newspeed/src/main/java/com/example/newspeed/user/dto/request/UserLoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.newspeed.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserLoginRequestDto {
+
+    private String email;
+    private String password;
+
+}
