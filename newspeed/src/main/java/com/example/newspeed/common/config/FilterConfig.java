dce19ff81e2c275bc410bcbb5a10dad7861a0a05
JoeMinKyung <mk981015@naver.com>
2025-02-17 11:23:24 +0900

    feat: login

--- newspeed/src/main/java/com/example/newspeed/common/config/FilterConfig.java
+++ newspeed/src/main/java/com/example/newspeed/common/config/FilterConfig.java
@@ -9,14 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-        @Bean
-        public FilterRegistrationBean loginFilter() {
-            FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-            filterRegistrationBean.setFilter(new LoginFilter());
-            filterRegistrationBean.setOrder(1);
-            filterRegistrationBean.addUrlPatterns("/*");
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
 
-            return filterRegistrationBean;
-        }
+        return filterRegistrationBean;
     }
 }
--- newspeed/src/main/java/com/example/newspeed/user/controller/AuthController.java
+++ newspeed/src/main/java/com/example/newspeed/user/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.example.newspeed.user.controller;
 
+import com.example.newspeed.common.consts.Const;
 import com.example.newspeed.user.dto.request.UserLoginRequestDto;
 import com.example.newspeed.user.dto.response.UserLoginResponseDto;
 import com.example.newspeed.user.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +23,9 @@ public class AuthController {
             @RequestBody UserLoginRequestDto dto,
             HttpServletRequest request
     ) {
-
         UserLoginResponseDto result = authService.login(dto);
+        HttpSession session = request.getSession();
+        session.setAttribute(Const.LOGIN_USER, result.getUserId());
         return ResponseEntity.ok("로그인 성공");
     }
 }
