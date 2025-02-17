a32256659defbcb2a193132d074d073052dc175d
JoeMinKyung <mk981015@naver.com>
2025-02-17 15:10:43 +0900

    feat: 본인 게시물에는 좋아요 눌렀을 때 오류 메세지 추가

--- newspeed/src/main/java/com/example/newspeed/like/service/LikeService.java
+++ newspeed/src/main/java/com/example/newspeed/like/service/LikeService.java
@@ -30,7 +30,7 @@ public class LikeService {
                 .orElseThrow(() -> new IllegalStateException("게시물을 찾을 수 없습니다."));
 
         if (post.getUser().getId().equals(userId)) {
-            throw new IllegalStateException("본인 게시물에는 좋아요를 누를 수 없습니다.");
+            throw new IllegalStateException("본인이 작성한 글에는 좋아요를 누를 수 없습니다.");
         }
 
         boolean liked;
