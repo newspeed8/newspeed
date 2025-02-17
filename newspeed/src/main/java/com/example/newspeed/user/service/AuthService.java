9d8bbca5c9458e912b7d1ce9e7c3d930b444062c
JoeMinKyung <mk981015@naver.com>
2025-02-17 11:50:23 +0900

    feat: exception 처리

--- newspeed/src/main/java/com/example/newspeed/user/service/AuthService.java
+++ newspeed/src/main/java/com/example/newspeed/user/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.newspeed.user.service;
 
+import com.example.newspeed.exception.InvalidCredentialException;
 import com.example.newspeed.user.dto.request.UserLoginRequestDto;
 import com.example.newspeed.user.dto.response.UserLoginResponseDto;
 import com.example.newspeed.user.entity.User;
@@ -17,16 +18,16 @@ public class AuthService {
     @Transactional(readOnly = true)
     public UserLoginResponseDto login(UserLoginRequestDto dto) {
         User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(
-                () -> new IllegalStateException("이메일이 존재하지 않습니다.")
+                () -> new InvalidCredentialException("이메일이 존재하지 않습니다.")
         );
 
         String password = dto.getPassword();
         if (password == null) {
-            throw new IllegalStateException("비밀번호를 입력해주세요.");
+            throw new InvalidCredentialException("비밀번호를 입력해주세요.");
         }
 
         if (!password.equals(user.getPassword())) {
-            throw new IllegalStateException("비밀번호가 틀렸습니다.");
+            throw new InvalidCredentialException("비밀번호가 틀렸습니다.");
         }
         return new UserLoginResponseDto(user.getId());
     }
