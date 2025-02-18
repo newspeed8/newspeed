51c91f5f6121e7fea44fd71d2b023b1d39b3af39
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-02-18 19:15:30 +0900

    chore: 필드명 오류 수정

--- newspeed/src/main/java/com/example/newspeed/comment/dto/response/CommentResponseDto.java
+++ newspeed/src/main/java/com/example/newspeed/comment/dto/response/CommentResponseDto.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 public class CommentResponseDto {
     private final Long id;
     private final Long userId;
-    private final Long scheduleId;
+    private final Long postId;
     private final String content;
 
-    public CommentResponseDto(Long id, Long userId, Long scheduleId, String content) {
+    public CommentResponseDto(Long id, Long userId, Long postId, String content) {
         this.id = id;
         this.userId = userId;
-        this.scheduleId = scheduleId;
+        this.postId = postId;
         this.content = content;
     }
 }
