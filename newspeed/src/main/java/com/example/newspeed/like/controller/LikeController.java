c8401dde8aead8da59b04c3f8428d19730bfc474
JoeMinKyung <mk981015@naver.com>
2025-02-19 09:38:57 +0900

    feat: 댓글 좋아요 추가

--- newspeed/src/main/java/com/example/newspeed/like/controller/LikeController.java
+++ newspeed/src/main/java/com/example/newspeed/like/controller/LikeController.java
@@ -32,20 +32,18 @@ public class LikeController {
         return ResponseEntity.ok(response);
     }
 
-    //댓글
-//    @PostMapping("/comment/{commentId}")
-//    public ResponseEntity<String> likeComment(
-//            HttpServletRequest request,
-//            @PathVariable Long commentId
-//    ) {
-//        HttpSession session = request.getSession(false);
-//        if (session == null || session.getAttribute(Const.LOGIN_USER) == null) {
-//            return ResponseEntity.status(401).body("로그인이 필요합니다.");
-//        }
-//
-//        Long userId = (Long) session.getAttribute(Const.LOGIN_USER);
-//        likeService.toggleCommentLike(userId, commentId);
-//
-//        return ResponseEntity.ok("댓글 좋아요 상태가 변경되었습니다.");
-//    }
+    @PostMapping("/comment/{commentId}")
+    public ResponseEntity<LikeResponseDto> likeComment(
+            HttpServletRequest request,
+            @PathVariable Long commentId
+    ) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(Const.LOGIN_USER) == null) {
+            return ResponseEntity.status(401).body(null);
+        }
+
+        Long userId = (Long) session.getAttribute(Const.LOGIN_USER);
+        LikeResponseDto response = likeService.toggleCommentLike(userId, commentId);
+        return ResponseEntity.ok(response);
+    }
 }
