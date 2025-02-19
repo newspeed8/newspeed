f2a20bdd6453910692af11958279720dcfb41996
HoKirby <lakevely27@gmail.com>
2025-02-19 19:37:03 +0900

    feat: Post 테이블의 좋아요수 column 기준으로 게시물 정렬 기능 수정

--- newspeed/src/main/java/com/example/newspeed/friend/entity/Friend.java
+++ newspeed/src/main/java/com/example/newspeed/friend/entity/Friend.java
@@ -6,7 +6,6 @@ import com.example.newspeed.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Table(name = "friend")
